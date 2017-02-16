0bf4373a8d49ffefcd4925299a0d3a7d48fc3d50
rbankston <rbankston@engineyard.com>
2017-02-16 11:31:59 -0500

    Adding deps.get

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,3 +1,3 @@
-run! "cd #{config.latest_release} && MIX_ENV=prod mix release"
+run! "cd #{config.latest_release} && MIX_ENV=prod mix deps.get && MIX_ENV=prod mix release"
 
 sudo "monit restart phoenix_#{config.app}"
