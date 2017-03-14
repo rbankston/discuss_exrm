a282c6e21b09a069c9974c493d10558754c7e048
Ralph Bankston <ralph.l.bankston@gmail.com>
2017-03-14 00:34:11 -0400

    Update after_symlink.rb

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,5 @@
 run! "cd #{config.latest_release} && MIX_ENV=prod mix deps.get"
 run! "cd #{config.latest_release} && MIX_ENV=prod mix release"
+run! "rsync -avHl #{config.latest_release}/rel/ #{config.shared}/rel/"
 
 sudo "monit restart phoenix_#{config.app}"
